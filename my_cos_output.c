3b14e4628f694555979f8494c362df2038acab5e
yyyhssss <huang_junxi@126.com>
2018-03-02 15:24:01 +0800

    Create my_cos_output.c

---
+++ my_cos_output.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+main()
+{
+double y;
+int x,m;
+for(y=1; y>=-1; y-=0.1)
+  {
+  m=acos(y)*10;
+  for(x=0; x<m; x++)
+    {
+    printf(" ");
+    }
+  printf("*");
+  for( ;x<(62-m); x++)
+    {printf(" ");}
+  print("*\n");
+  }
+}
