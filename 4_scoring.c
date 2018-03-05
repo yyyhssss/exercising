bdd8581c09bf8091f0e4e835bbbb3288ff227b81
yyyhssss <huang_junxi@126.com>
2018-03-05 13:32:41 +0800

    Update 4_scoring.c

--- 4_scoring.c
+++ 4_scoring.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-main
+#include <math.h>
+int main()
 {
   int integer[10], i, max, min, sum,n_best,n_worst,average;
   max=-32768;
@@ -10,18 +11,18 @@ main
   for(i=0; i<10; i++)
   {
     printf("Input number %d=", i+1);
-    scanf("%d", integer[i]);
+    scanf("%d", &integer[i]);
     sum+= integer[i];
-    if(integer[i]>max) max=integer;
-    if(integer[i]<min) min=integer;
+    if(integer[i]>max) max=integer[i];
+    if(integer[i]<min) min=integer[i];
   }
   average=(sum-max-min)/8;
   for(i=0; i<10; i++)
   {
-    if(abs(integer[i]-average)>abs(integer[n_worst]-average]) n_worst=i;
-    if(abs(integer[i]-average)<abs(integer[n_best]-average]) n_best=i;
+    if(abs(integer[i]-average)>abs(integer[n_worst]-average)) n_worst=i;
+    if(abs(integer[i]-average)<abs(integer[n_best]-average)) n_best=i;
   }
   printf("Canceled max score: %d\nCanceled min score %d\n", max, min);
-  printf("Average score: %d", (sum-max-min)/8);
+  printf("Average score: %d\n", average);
   printf("Best judge: number %d\nWorst judge: number %d\n", n_best, n_worst);
 }
