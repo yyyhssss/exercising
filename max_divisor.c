c420fde2b03aa4e32ca59e4e97b55326dcea4e60
yyyhssss <huang_junxi@126.com>
2018-03-05 11:19:42 +0800

    Create max_divisor.c

---
+++ max_divisor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main
+{
+long i;
+int j;
+printf("Please input a number:");
+scanf("%ld", i)
+for(j=999; j>=100; j--)
+{
+  if(i%j==0)
+  {
+    printf("The max 3 digit divisor of %ld is %d\n", i, j);
+    break;
+  }
+}
+if(j<100) printf("There is no 3 digit divisor of %ld", i);
+}
