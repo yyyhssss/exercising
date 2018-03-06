777d227e25945332455e2a084c23029c4c299f6a
yyyhssss <huang_junxi@126.com>
2018-03-06 09:36:22 +0800

    Update 8_borrow_books.c

--- 8_borrow_books.c
+++ 8_borrow_books.c
@@ -2,13 +2,13 @@
 int main()
 {
 int i,j,k,count=1;
-for(i=0; i<5; i++)
+for(i=1; i<=5; i++)
 {
-  for(j=0; j<5; j++)
+  for(j=1; j<=5; j++)
   {
-    for(k=0; k<5; k++)
+    for(k=1; i!=j && k<=5; k++)
     {
-      if(i!=j && i!=k && j!=k)
+      if(i!=k && j!=k)
       {printf(count%8?"%2d: %d %d %d " : "%2d: %d %d %d\n", ++count,i,j,k);}
     }
   }
