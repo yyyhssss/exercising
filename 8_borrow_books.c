4ac2dfc642155e6f3eb92563bdc1d389a5de2237
yyyhssss <huang_junxi@126.com>
2018-03-06 09:34:56 +0800

    Update 8_borrow_books.c

--- 8_borrow_books.c
+++ 8_borrow_books.c
@@ -6,12 +6,10 @@ for(i=0; i<5; i++)
 {
   for(j=0; j<5; j++)
   {
-    while(j==i) j++;
     for(k=0; k<5; k++)
     {
-      while(k==j || k==i) k++;
-      printf("Method %d: %d %d %d  ", count,i,j,k);
-      count++;
+      if(i!=j && i!=k && j!=k)
+      {printf(count%8?"%2d: %d %d %d " : "%2d: %d %d %d\n", ++count,i,j,k);}
     }
   }
 }
