726fe766e7aff29153baae70696129ff9c36d76c
yyyhssss <huang_junxi@126.com>
2018-03-11 00:29:24 +0800

    Update 34_prime_circle_zhu.c

--- 34_prime_circle_zhu.c
+++ 34_prime_circle_zhu.c
@@ -2,32 +2,44 @@
 #include<iostream>
 #include<cstdlib>
 #include<cmath>
+#include <time.h>
+#include <stdio.h>
+
+#define N 18
+
 using namespace std;
-bool b[21]={0,1};
+bool b[N+1]={0,1};
 int total=0;
-int a[21]={0,1};
+int a[N+1]={0,1};
 void search(int t);
 void print();
 bool pd(int,int);
 int main()
 {
+    time_t start,end;
+    double cost;
+    time(&start);
     search(2);
     cout<<total;
+    cout<<endl;
+    time(&end);
+    cost=difftime(end,start);
+    printf("cost is %f\n",cost);
     getchar();
     return 0;
 } 
 void search( int t )
 {
     int i;
-    for (i=1;i<11;i++)
+    for (i=1;i<(N+1);i++)
     {
         if (pd(a[t-1],i)&&(!b[i]))// 原来是i-1,实际是t-1
         {
             a[t]=i;
             b[i]=1;
-            if(t==10)  
+            if(t==N)  
             {
-                if (pd(a[10],a[1])) 
+                if (pd(a[N],a[1])) 
                 {
                     print();
                 }
@@ -44,7 +56,7 @@ void print()
 {
     total++;
     cout<<"<"<<total<<">";
-    for (int j=1;j<=10;j++)
+    for (int j=1;j<=N;j++)
     {
         cout<<a[j]<<" ";
     }
