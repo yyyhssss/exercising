4f60bafd6c9a913a48e8e13e546f704a10a771d3
yyyhssss <huang_junxi@126.com>
2018-03-15 21:06:45 +0800

    Update

--- 35_16_cube.c
+++ 35_16_cube.c
@@ -5,58 +5,76 @@
 
 int b[17]={0};
 int total=0;
-int a[5][5]={0};
+int a[17]={0};
 void search(int t);
 void print();
 int pd(int,int);
 int main()
 {
+    int i;
     time_t start,end;
     double cost=0;
-    int i,j;
     time(&start);
-    for(i=1;i<=4;i++)
-      for(j=1;j<=4;j++)
-	search(i,j);
+    for(i=1;i<=11;i++)
+    {
+      a[1]=i;
+      b[i]=1;
+      search(2);
+      b[i]=0;
+    }
+    //search(1);
     time(&end);
     cost=difftime(end,start);
     printf("cost is %f\n",cost);
     return 0;
 } 
-void search( int i, int j )
+void search( int n)
 {
-    int m,n;
+    int i;
     for (i=1;i<=16;i++)
     {
-        if (pd(a[t-1],i)&&(!b[i]))// 原来是i-1,实际是t-1
-        {
-            a[t]=i;
-            b[i]=1;
-            if(t==N)  
-            {
-                if (pd(a[N],a[1])&&a[N]>a[2]) 
-                {
-                    print();
-                }
-            }
-            else
-            {
-                search(t+1);
-            }
-            b[i]=0;
-        }
+      if(b[i])	continue;     //如果该数存在，继续寻找下一个
+
+      if(n<5)
+      {
+	if(n==4)
+	  if(i<a[1])	continue;   //如果右上角>左上角，寻找下一个
+
+	if( pd(a[n-1],i) )
+	{
+	  a[n]=i;
+	  b[i]=1;
+	  search(n+1);
+	  b[i]=0;
+	}
+      }
+      else
+      {
+	if(n==13)
+	  if(i<a[4])	continue;          //如果左下角>右上角，寻找下一个
+
+	if(n%4==1? pd(a[n-4],i): (pd(a[n-1],i) && pd(a[n-4],i)) )
+	{
+	  a[n]=i;
+	  b[i]=1;
+	  if(n==16 && i>a[13])
+	    print();
+	  else
+	    search(n+1);
+	  b[i]=0;
+	}
+      }
     }
 }
 void print()
 {
     total++;
-    //cout<<"<"<<total<<">";
-    //for (int j=1;j<=N;j++)
-    //{
-    //    cout<<a[j]<<" ";
-    //}
+    printf("%d\n",total);
+    for (int j=1;j<=N;j++)
+    {
+      printf(j%4? "%3d":"%3d\n", a[j]);
+    }
  
-    //cout<<endl;
 }
 int pd(int x,int y)
 {
